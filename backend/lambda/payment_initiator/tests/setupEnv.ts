e2c523d6c1e1ef178320eb4a31229d1a2bd72c77
hneko <>
2025-12-05 14:21:11 +0900

    feat: 🎸 add jest

---
+++ backend/lambda/payment_initiator/tests/setupEnv.ts
@@ -0,0 +1,5 @@
+process.env.TARGET_ENV = 'jest';
+process.env.TRANSACTIONS_TABLE = 'local-TransactionsTable';
+process.env.PRODUCTS_TABLE = 'local-ProductsTable';
+process.env.STEP_FUNCTION_LAMBDA_ARN = 'testArn';
+process.env.STRIPE_SECRET_KEY = 'testKey';
