4fc3f57f6526fb6e0148b47188cc004cdb38fa84
hneko <>
2025-12-10 14:37:45 +0900

    refactor: 💡 for test

--- backend/01-lambda.sh
+++ backend/01-lambda.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-cd lambda/payment_initiator
-npm run build
+(
+  cd lambda/payment_initiator
+  npm run build
+) &
+
+(
+  cd lambda/payment_invoke
+  npm run build
+) &
+
+wait
+echo "All builds completed!"
